29a537b88b808745833f54173e75e778c32a0edb
Chris Quinn <chris@gravitywell.co.uk>
2016-07-15 13:21:09 +0100

    standardised reflect to use body params, added reflect-more endpoint for more verbose output

--- server/main.js
+++ server/main.js
@@ -7,39 +7,48 @@ var Api = new Restivus({
   prettyJson: true
 });
 
-Api.addRoute('checkstat', {authRequired: false}, {
-
-  post: function () {
-
-    let params = this.bodyParams;
-    let date = new Date();
-
-    if(params){
-      if(params.invoker){
-        console.log('Checkstat called by ' + params.invoker + ' at ' + date); // add requester
-      } else {
-        console.log('Checkstat called by Anonymous at ' + date);
-      }
-    }
-
-    return {
-      success: true,
-      timestamp: date
-    };
-  }
-
-});
-
 Api.addRoute('reflect', {authRequired: false}, {
 
   post: function () {
+    return this.bodyParams;
+  },
 
-    let params = this.bodyParams;
-    console.log(params);
-    console.log(params.chat_id);
-    
-    return params;
+  get: function () {
+    return this.bodyParams;
   }
 
 });
+
+Api.addRoute('reflect-more', {authRequired: false}, {
+
+  post: function () {
+    return reflectMore(this.bodyParams);
+  },
+
+  get: function () {
+    return reflectMore(this.bodyParams);
+  }
+
+});
+
+var reflectMore = (params)=>{
+  
+  let date = new Date();
+  
+  if(params){
+    if(params.invoker){
+      console.log('Checkstat called by ' + params.invoker + ' at ' + date); // add requester
+    } else {
+      console.log('Checkstat called by Anonymous at ' + date);
+    }
+  }
+  return {
+    params: params,
+    success: true,
+    timestamp: date
+  };
+};
+
+
+
 
